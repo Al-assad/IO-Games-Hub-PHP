2920dec8d4986a14e496208c1d46971727cabd49
Al-assad <yulinying_1994@outlook.com>
2017-02-20 17:11:26 +0800

    update

--- iogame/application/views/basic_module/canvas_nest.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**Created by Intellij IDEA
- * Author: Al-assad
- * E-mail: yulinying_1994@outlook.com
- * Github: https://github.com/Al-assad
- * Date: 2017/2/16 14:22
- * Description: canvas-nest 的视图
- */?>
-<script opacity="0.4" zIndex="-1" count="<?php echo $canvas_count; ?>" src="<?php echo $base_url; ?>libraries/canvas-nest.min.js"></script>
